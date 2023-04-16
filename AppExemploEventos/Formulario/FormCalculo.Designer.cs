2840c09d3330d2e76adf32ff1efe0412556004d0
Davi_Lobato <davimlobato7@gmail.com>
2023-04-16 19:27:53 -0400

    Criado

---
+++ AppExemploEventos/Formulario/FormCalculo.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace AppExemploEventos.Formulario
+{
+    partial class FormCalculo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbLadoC = new System.Windows.Forms.Label();
+            this.lbLadoB = new System.Windows.Forms.Label();
+            this.lbLadoA = new System.Windows.Forms.Label();
+            this.btNovo = new System.Windows.Forms.Button();
+            this.btCalcular = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.txtValor2 = new System.Windows.Forms.TextBox();
+            this.txtValor1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbLadoC
+            // 
+            this.lbLadoC.AutoSize = true;
+            this.lbLadoC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLadoC.Location = new System.Drawing.Point(87, 379);
+            this.lbLadoC.Name = "lbLadoC";
+            this.lbLadoC.Size = new System.Drawing.Size(143, 25);
+            this.lbLadoC.TabIndex = 38;
+            this.lbLadoC.Text = "RESULTADO";
+            // 
+            // lbLadoB
+            // 
+            this.lbLadoB.AutoSize = true;
+            this.lbLadoB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLadoB.Location = new System.Drawing.Point(82, 169);
+            this.lbLadoB.Name = "lbLadoB";
+            this.lbLadoB.Size = new System.Drawing.Size(110, 25);
+            this.lbLadoB.TabIndex = 37;
+            this.lbLadoB.Text = "VALOR 2:";
+            // 
+            // lbLadoA
+            // 
+            this.lbLadoA.AutoSize = true;
+            this.lbLadoA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLadoA.Location = new System.Drawing.Point(82, 83);
+            this.lbLadoA.Name = "lbLadoA";
+            this.lbLadoA.Size = new System.Drawing.Size(110, 25);
+            this.lbLadoA.TabIndex = 36;
+            this.lbLadoA.Text = "VALOR 1:";
+            // 
+            // btNovo
+            // 
+            this.btNovo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btNovo.Location = new System.Drawing.Point(396, 287);
+            this.btNovo.Name = "btNovo";
+            this.btNovo.Size = new System.Drawing.Size(242, 55);
+            this.btNovo.TabIndex = 35;
+            this.btNovo.Text = "NOVO CÁLCULO";
+            this.btNovo.UseVisualStyleBackColor = true;
+            // 
+            // btCalcular
+            // 
+            this.btCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCalcular.Location = new System.Drawing.Point(87, 287);
+            this.btCalcular.Name = "btCalcular";
+            this.btCalcular.Size = new System.Drawing.Size(242, 55);
+            this.btCalcular.TabIndex = 34;
+            this.btCalcular.Text = "CALCULAR";
+            this.btCalcular.UseVisualStyleBackColor = true;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(87, 407);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(178, 34);
+            this.txtResultado.TabIndex = 33;
+            // 
+            // txtValor2
+            // 
+            this.txtValor2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor2.Location = new System.Drawing.Point(87, 197);
+            this.txtValor2.Name = "txtValor2";
+            this.txtValor2.Size = new System.Drawing.Size(178, 34);
+            this.txtValor2.TabIndex = 32;
+            // 
+            // txtValor1
+            // 
+            this.txtValor1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor1.Location = new System.Drawing.Point(87, 111);
+            this.txtValor1.Name = "txtValor1";
+            this.txtValor1.Size = new System.Drawing.Size(178, 34);
+            this.txtValor1.TabIndex = 31;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(764, 46);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "DIVISÃO";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormCalculo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 481);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbLadoC);
+            this.Controls.Add(this.lbLadoB);
+            this.Controls.Add(this.lbLadoA);
+            this.Controls.Add(this.btNovo);
+            this.Controls.Add(this.btCalcular);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtValor2);
+            this.Controls.Add(this.txtValor1);
+            this.Name = "FormCalculo";
+            this.Text = "FormCalculo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbLadoC;
+        private System.Windows.Forms.Label lbLadoB;
+        private System.Windows.Forms.Label lbLadoA;
+        private System.Windows.Forms.Button btNovo;
+        private System.Windows.Forms.Button btCalcular;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.TextBox txtValor2;
+        private System.Windows.Forms.TextBox txtValor1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ AppExemploEventos/Formulario/FormMensagem.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppExemploEventos.Formulario
+{
+    public partial class FormMensagem : Form
+    {
+        public FormMensagem()
+        {
+            InitializeComponent();
+        }
+
+        private void btAlerta_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mensagem da caixa", "Tiítulo da Caixa", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+        }
+
+        private void btInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mensagem da caixa", "Título da caixa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btErro_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mensagem da caixa", "Título da caixa", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void btPergunta_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mensagem da caixa", "Título da caixa", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+        }
+
+        private void btResposta_Click(object sender, EventArgs e)
+        {
+            DialogResult resp;
+            string pergunta = "Pergunta?";
+            string titulo = "ADS 2022-2";
+            string resposta;
+            resp = MessageBox.Show(pergunta, titulo, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (resp == DialogResult.Yes)
+            {
+                resposta = "Sim";
+                MessageBox.Show(resposta, titulo, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                resposta = "nao";
+                MessageBox.Show(resposta, titulo, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
